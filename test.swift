ad5dfbac8eada3d0bb3816626e9b8c6007f6dc60
DSA1018 <66327767+DSA1018@users.noreply.github.com>
2024-12-27 09:08:00 -0800

    Implemented terminal-based skeleton in Swift

--- test.swift
+++ test.swift
@@ -1,1 +1,30 @@
-print("hello")
+func make_board () -> [[Int]] {
+    var board:[[Int]] = []
+    var row_count = 1
+    while row_count < 10 {
+        var col_count = 1
+        var row: [Int] = []
+        while col_count < 10 {
+            row.append(col_count)
+            col_count = col_count + 1
+        }
+        board.append(row)
+        row_count = row_count + 1
+    }
+    return board
+}
+
+func print_board(board: [[Int]]) {
+    print("----------------------------------------")
+    for row in board{
+        for cell in row {
+            print ("| " + String(cell), terminator: " ")
+        }
+        print("|")
+        print("----------------------------------------")
+    }
+}
+
+let board: [[Int]] = make_board()
+
+print_board(board: board)
